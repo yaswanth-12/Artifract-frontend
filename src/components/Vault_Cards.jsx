50cec61b810ad68fc1ac47241f9922e6ba52ff5c
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-04-16 01:22:39 +0530

    1.22am

--- src/components/Vault_Cards.jsx
+++ src/components/Vault_Cards.jsx
@@ -9,14 +9,18 @@ export default function Cards() {
                 <div className={styles.flip_container}>
                     <div className={styles.flipper}>
                         <img src="/2.avif" alt="Image" className={styles.front} />
-                        <img src="/1.avif" alt="Image Flipped" className={styles.back} />
+                        <img src="/BAYC.png" alt="Image Flipped" className={styles.back} />
                     </div>
                 </div>
             </div>
 
             <div className={styles.container}>
                 <div className={styles.col1}>
-                    <h3> Desposit </h3>
+                    <h3> Desposit: </h3>
+                    <p>
+                    Deposit your BAYC
+                    token into the vault.
+                    </p>
                 </div>
                 <div className={styles.col2}>
                     <div className={styles.deposit}>
@@ -24,7 +28,7 @@ export default function Cards() {
                             <input type="text" placeholder="Token ID" className={styles.inputbox}></input>
                             <input type="text" placeholder="No of fractions" className={styles.inputbox} ></input>
                             <input type="text" placeholder="minimum price" className={styles.inputbox} ></input>
-                            <button className={styles.submitButton}>Submit</button>
+                            <button className={styles.submitButton}>Deposit</button>
                         </form>
                     </div>
                 </div>
@@ -32,36 +36,36 @@ export default function Cards() {
 
             <div className={styles.container1}>
                 <div className={styles.col11}>
-                    <h3> Initiate_buyout </h3>
+                    <h3> Initiate BuyOut </h3>
                 </div>
                 <div className={styles.col22}>
                     <div className={styles.initiate_buyout}>
                         <input type="text" placeholder="TokenID" className={styles.inputbox} ></input>
-                        <button className={styles.submitButton}>Submit</button>
+                        <button className={styles.submitButton}>BuyOut</button>
                     </div>
                 </div>
             </div>
 
             <div className={styles.container1}>
                 <div className={styles.col11}>
-                    <h3> End_bid </h3>
+                    <h3> Bid on NFT </h3>
                 </div>
                 <div className={styles.col22}>
                     <div className={styles.end_bid}>
                         <input type="text" placeholder="tokenID" className={styles.inputbox} ></input>
-                        <button className={styles.submitButton}>Submit</button>
+                        <button className={styles.submitButton}>Bid on NFT </button>
                     </div>
                 </div>
             </div>
 
             <div className={styles.container1}>
                 <div className={styles.col11}>
-                    <h3> bid_onNFT </h3>
+                    <h3> End Bid  </h3>
                 </div>
                 <div className={styles.col22}>
                     <div className={styles.bid_onNFT}>
                         <input type="text" placeholder="tokenID" className={styles.inputbox} ></input>
-                        <button className={styles.submitButton}>Submit</button>
+                        <button className={styles.submitButton}>End Bid</button>
                     </div>
                 </div>
             </div>
@@ -73,7 +77,7 @@ export default function Cards() {
                 <div className={styles.col22}>
                     <div className={styles.withdraw}>
                         <input type="text" placeholder="tokenID" className={styles.inputbox} ></input>
-                        <button className={styles.submitButton}>Submit</button>
+                        <button className={styles.submitButton}> Withdraw </button>
                     </div>
                 </div>
             </div>
