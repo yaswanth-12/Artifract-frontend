591f5cb5fbbaa3819a7cfb92fcc05a3e082e826e
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-04-15 17:01:47 +0530

    5.01

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,6 @@ export default function Main() {
     return (
         <>
             <div className={styles.main}>
-                <h3>We Are Creative</h3>
                 <span>THE WAY OF </span> <span className={styles.red}>DREAM</span>
                 <p>A single place to share, curate and discover visual that tells a story.</p> <br />
                 <button className={styles.button1}>LEARN MORE</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,6 @@ export default function Landing_Navbar() {
           <img src='/Artifract_Logo.png' ></img>
           <a href="#" className={styles.red}> <li> Home </li> </a>
           <a href="#" > <li> Ecosystem </li> </a>
-          <a href="#" > <li> Community </li> </a>
-          <a href="#" > <li> Developers </li> </a>
-          <a href="#" > <li> FAQ </li> </a>
           <a href="#" > <li> Contact </li> </a>
           <Link href="/home">
             <button className={styles.glow_btn}>Launch App</button>
