d80f9efc7ed4be5d0be2d6cd631c0f42440ae239
Yaswanth Gadamsetti <91465963+yaswanth-12@users.noreply.github.com>
2023-04-16 04:07:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Landing_Navbar() {
   return (
     <>
       <title>Artifract ♥</title>
-      <link rel="icon" href="/Artifract_Logo_White.png"></link>
+      <link rel="icon" href="https://yaswanth-12.github.io/Artifract-frontend/Artifract_Logo_White.png"></link>
       <div className={styles.nav}>
         <ul>
-          <img src='/Artifract_Logo.png' ></img>
+          <img src='https://yaswanth-12.github.io/Artifract-frontend/Artifract_Logo.png' ></img>
           <a href="#" className={styles.red}> <li> Home </li> </a>
           <a href="#" > <li> Ecosystem </li> </a>
           <a href="#" > <li> Contact </li> </a>
