3289b82b1cd6d4434dd8c4b60672361560fb146e
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-04-15 23:30:38 +0530

    11.30

--- src/components/Vault_Cards.jsx
+++ src/components/Vault_Cards.jsx
@@ -7,11 +7,31 @@ export default function Cards() {
         <>
             <div className={styles.container}>
                 <div className={styles.col1}>
-                    <img src={"/2.avif"} className={styles.image}></img>
+                    <div className={styles.flip_container}>
+                        <div className={styles.flipper}>
+                            <img src="/2.avif" alt="Image" className={styles.front} />
+                            <img src="/1.avif" alt="Image Flipped" className={styles.back} />
+                        </div>
+                    </div>
+
                 </div>
 
                 <div className={styles.col2}>
-                    
+                    <div className={styles.deposit}>
+                        tokenID, no of fractions, minimum buyout wait time, minimum price.
+                    </div>
+                    <div className={styles.initiate_buyout}>
+                            tokenID
+                    </div>
+                    <div className={styles.bid_onNFT}>
+                            tokenID
+                    </div>
+                    <div className={styles.end_bid}>
+                            tokenID
+                    </div>
+                    <div className={styles.withdraw}>
+                            tokenID
+                    </div>
                 </div>
             </div>
         </>
