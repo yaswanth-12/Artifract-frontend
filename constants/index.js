b5502993f423bfc3f605eb36f4cca4f853addc5c
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-04-16 10:20:20 +0530

    10.20am

---
+++ constants/index.js
@@ -0,0 +1,287 @@
+export const VAULT_ADDRESS = '0xAa4979E4E857d93EF3e86BCd0a16Ca199FE2bB63';
+export const VAULT_ABI= [
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferStarted",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "acceptOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "claimContract",
+        "outputs": [
+            {
+                "internalType": "contract claimBuyOuts",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "contract IERC721",
+                "name": "_address",
+                "type": "address"
+            }
+        ],
+        "name": "deployVault",
+        "outputs": [
+            {
+                "internalType": "contract Vault",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "pendingOwner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "contract claimBuyOuts",
+                "name": "_claimContract",
+                "type": "address"
+            }
+        ],
+        "name": "updateClaimContract",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "vaultRegistry",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+export const CLAIM_ADDRESS = '0xc739ECB2B7d1C4E4D0fe90ef9999705a0e070a47';
+export const CLAIM_ABI= [
+    {
+        "inputs": [
+            {
+                "internalType": "contract VaultFactory",
+                "name": "_vaultFactory",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "contract IERC721",
+                "name": "_NftContract",
+                "type": "address"
+            }
+        ],
+        "name": "claimAllFailedNft",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_NftContract",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_tokenId",
+                "type": "uint256"
+            }
+        ],
+        "name": "claimBalance",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "contract IERC721",
+                "name": "_NftContract",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_tokenId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address",
+                "name": "_bidder",
+                "type": "address"
+            }
+        ],
+        "name": "depositFailedTransferNft",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "failedNftClaim",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "data",
+                "type": "bytes32"
+            }
+        ],
+        "name": "receiveDeposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "name": "totalClaimAmount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
--- src/components/hNavbar.jsx
+++ src/components/hNavbar.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from '../styles/navbar.module.css';
-import { ethers } from "ethers";
-import { parseEther } from "ethers/utils";
+import ethers,{parseEther, Contract} from "ethers";
+import {  BrowserProvider } from "ethers/providers"
+import { VAULT_ABI, VAULT_ADDRESS } from '../../constants';
+
+let account;
 
 function WalletConnect() {
   const connectButton = useRef(null);
-  const [transactionHash, setTransactionHash] = useState(null);
-
 
   useEffect(() => {
     // Web3 Browswer Detection
@@ -14,53 +15,49 @@ function WalletConnect() {
       console.log("Injected Web3 Wallet is installed!");
     }
 
+    //Click Event
+    connectButton.current.addEventListener("click", () => {
+      connectAccount();
+    });
+
+    //Connect Account Function
     async function connectAccount() {
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
-      const account = accounts[0];
-      connectButton.current.innerHTML = `Address: ${account.slice(0, 6)}...${account.slice(-4)}`;
-
+      account = accounts[0];
+      connectButton.current.innerHTML = "Address: " +
+        account[0] +
+        account[1] +
+        account[2] +
+        account[3] +
+        account[4] +
+        account[5] +
+        "..." +
+        account[38] +
+        account[39] +
+        account[40] +
+        account[41];
       if (typeof window.ethereum !== "undefined") {
-        const provider = new ethers.BrowserProvider(window.ethereum);
+        const provider = new BrowserProvider(ethereum);
         await provider.send("eth_requestAccounts", []);
-      }
-    }
-    connectButton.current.addEventListener("click", connectAccount);
+        let signer = await provider.getSigner();
+        
+        const contract = new Contract(VAULT_ADDRESS, VAULT_ABI, signer)
+        contr
 
-    return () => {
-      connectButton.current.removeEventListener("click", connectAccount);
-    };
+        const tx = await signer.sendTransaction({
+          from: await signer.getAddress(),
+          to: "0x92bA38bBB2B0f307b031Df22475578123c73E1F6",
+          value: parseEther("0.001")
+        }).then(result => console.log(result)).catch(err => {console.log(err)})
+      };
+
+    }
   }, [connectButton]);
 
-  async function demo() {
-    console.log("sdubcusdkcbukdbsb");
-    await ethereum.request({
-      method: "eth_sendTransaction",
-      params : {
-        from: "0xA42468079F9b7275C442253a86A9beC5F503eC5A",
-        to: "0x0",
-        data: "0x0",
-        value: parseEther('0.01')
-      }
-    });
-    // const account = accounts[0];
-    // const provider = new ethers.BrowserProvider(window.ethereum);
-
-    console.log("Connect");
-
-    // const transactionHash = await provider.send('eth_sendTransaction', params)
-    // setTransactionHash(transactionHash);
-    // console.log('transactionHash is ' + transactionHash);
-
-    console.log("Connected");
-  }
   return (
-    <>
-      <button ref={connectButton} className={styles.glow_btn}> Connect wallet </button>
-      <button onClick={demo}>Send Transaction</button>
-      {transactionHash && <p>Transaction Hash: {transactionHash}</p>}
-    </>
+    <button ref={connectButton} className={styles.glow_btn}> Connect wallet </button>
   );
 }
 
